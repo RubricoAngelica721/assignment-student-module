dbca61180cb89517e60cd2bd6a8d0abd5c852371
Jade Ryan Blancaflor <131427861+blancaflor480@users.noreply.github.com>
2025-01-10 12:38:59 +0800

    Implemented create student functionality

--- src/students/student_controller.ts
+++ src/students/student_controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Post, Get, Patch, Delete, Body, Param, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { StudentService } from './student_service';
 import { CreateStudentDto } from './table';
 import { Student } from './entity';
@@ -12,33 +22,5 @@ export class StudentController {
   @HttpCode(HttpStatus.CREATED)
   async create(@Body() createStudentDto: CreateStudentDto): Promise<Student> {
     return await this.studentService.create(createStudentDto);
-  }
-
-  // Get all students
-  @Get()
-  async findAll(): Promise<Student[]> {
-    return await this.studentService.findAll();
-  }
-
-  // Get a specific student by ID
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Student> {
-    return await this.studentService.findOne(+id);
-  }
-
-  // Update a student
-  @Patch(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() UpdateStudentDto: Partial<CreateStudentDto>
-  ): Promise<Student> {
-    return await this.studentService.update(+id, UpdateStudentDto);
-  }
-
-  // Delete a student
-  @Delete(':id')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async remove(@Param('id') id: string): Promise<void> {
-    return await this.studentService.remove(+id);
   }
 }
