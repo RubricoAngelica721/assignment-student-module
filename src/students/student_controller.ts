4ed27017fa194036d9a8c985f4181436f2b03340
Ma. Angelica M. Rubrico <87623792+RubricoAngelica721@users.noreply.github.com>
2025-01-10 12:47:37 +0800

    Update student_controller.ts

--- src/students/student_controller.ts
+++ src/students/student_controller.ts
@@ -1,14 +1,4 @@
-import {
-  Controller,
-  Post,
-  Get,
-  Patch,
-  Delete,
-  Body,
-  Param,
-  HttpCode,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Post, Get, Patch, Delete, Body, Param, HttpCode, HttpStatus } from '@nestjs/common';
 import { StudentService } from './student_service';
 import { CreateStudentDto } from './table';
 import { Student } from './entity';
@@ -17,10 +7,18 @@ import { Student } from './entity';
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  // Create a new student
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  async create(@Body() createStudentDto: CreateStudentDto): Promise<Student> {
-    return await this.studentService.create(createStudentDto);
+  // Get a specific student by ID
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Student> {
+    return await this.studentService.findOne(+id);
+  }
+
+  // Update a student
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() UpdateStudentDto: Partial<CreateStudentDto>
+  ): Promise<Student> {
+    return await this.studentService.update(+id, UpdateStudentDto);
   }
 }
