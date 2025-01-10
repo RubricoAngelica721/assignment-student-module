81d8eedd97d74c707522b098d04ef220ae194c5d
Ma. Angelica M. Rubrico <87623792+RubricoAngelica721@users.noreply.github.com>
2025-01-10 12:57:43 +0800

    Delete student_controller.ts

--- src/students/student_controller.ts
+++ src/students/student_controller.ts
@@ -1,26 +1,21 @@
-import {
-  Controller,
-  Post,
-  Get,
-  Patch,
-  Delete,
-  Body,
-  Param,
-  HttpCode,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Post, Get, Patch, Delete, Body, Param, HttpCode, HttpStatus } from '@nestjs/common';
 import { StudentService } from './student_service';
-import { CreateStudentDto } from './table';
 import { Student } from './entity';
 
 @Controller('students')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  // Create a new student
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  async create(@Body() createStudentDto: CreateStudentDto): Promise<Student> {
-    return await this.studentService.create(createStudentDto);
+  // Get a specific student by ID
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Student> {
+    return await this.studentService.findOne(+id);
+  }
+
+  // Delete a student
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id') id: string): Promise<void> {
+    return await this.studentService.remove(+id);
   }
 }
